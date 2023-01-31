89b37208ae6842797c809d7c045f23fa90a2afe9
codsp <codspecialops@gmail.com>
2023-01-31 07:01:13 -0600

    Updated GUI.py and did some coding exercises.

--- gui.py
+++ gui.py
@@ -2,14 +2,33 @@ from functions import read_tasks, write_tasks
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a task to do today")
-input_box = sg.InputText(tooltip="Enter task here")
+input_box = sg.InputText(tooltip="Enter task here", key="task")
 
 add_button = sg.Button("Add Task")
 edit_button = sg.Button("Edit Task")
 complete_button = sg.Button("Complete Task")
-exit_button = sg.Button("Exit Task")
+exit_button = sg.Button("Exit")
 
-desktop_window = sg.Window("Daily Task Manager", layout=[[label], [input_box, add_button]])
+desktop_window = sg.Window("Daily Task Manager",
+                           layout=[[label], [input_box, add_button]],
+                           font=("Helvetica", 12))
 
-desktop_window.read()
-desktop_window.close()
+while True:
+	event, values = desktop_window.read()
+	match event:
+		case "Add Task":
+			tasks = read_tasks()
+			new_task = values["task"] + "\n"
+
+			tasks.append(new_task)
+			write_tasks(tasks)
+
+		case "Edit Task":
+
+		case "Complete Task":
+
+		case "Exit":
+			break
+
+		case sg.WIN_CLOSED:
+			break
