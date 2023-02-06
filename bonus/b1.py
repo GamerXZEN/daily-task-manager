a010a56a714f744c4fdf69102df15112e204467e
FESL Sapphire Dev <107291366+GamerXZEN@users.noreply.github.com>
2023-02-05 18:16:58 -0600

    Delete bonus directory

--- bonus/b1.py
+++
@@ -1,9 +0,0 @@
-try:
-    width = float(input("Enter rectangle width: "))
-    length = float(input("Enter rectanlge length: "))
-    if width == length:
-        exit('Dp not enter squares next time.')
-    area = width * length
-    print(area)
-except ValueError:
-    print("Please enter a number")
